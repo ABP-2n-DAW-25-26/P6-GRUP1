359bcef0398656d1e0b22f301e91ca51056e3710
Bal-cpu8 <erbalvisan@gmail.com>
2026-04-09 17:29:36 +0200

    Route + controller + model to exchanges

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use Laravel\Fortify\Features;
+use App\Http\Controllers\ExchangeController;
 
 Route::inertia('/', 'Welcome', [
     'canRegister' => Features::enabled(Features::registration()),
@@ -10,5 +11,6 @@ Route::inertia('/', 'Welcome', [
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::inertia('dashboard', 'Dashboard')->name('dashboard');
 });
+Route::resource('exchange', ExchangeController::class);
 
 require __DIR__.'/settings.php';
