c9dfb37a0b9f07a16533affc104677bfcdc24842
jeffffff6 <lolbak006@gmail.com>
2026-04-15 16:26:07 +0200

    feat: user seeder

--- resources/js/pages/Welcome.vue
+++ resources/js/pages/Welcome.vue
@@ -154,6 +154,7 @@ withDefaults(
                         </li>
                     </ul>
                 </div>
+                <div class="text-sky-300"> hol</div>
                 <div
                     class="relative -mb-px aspect-[335/364] w-full shrink-0 overflow-hidden rounded-t-lg bg-[#fff2f2] lg:mb-0 lg:-ml-px lg:aspect-auto lg:w-[438px] lg:rounded-t-none lg:rounded-r-lg dark:bg-[#1D0002]"
                 >
