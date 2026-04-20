8d127f76781c37baa439fb03f01088fced29ef8d
Bal-cpu8 <erbalvisan@gmail.com>
2026-04-20 16:20:14 +0200

    Add map with leaflet

---
+++ resources/js/components/LeafletMap.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="map" class="w-full h-full rounded-xl"></div>
+</template>
+
+<script setup>
+import { onMounted } from 'vue'
+import L from 'leaflet'
+import 'leaflet/dist/leaflet.css'
+
+onMounted(() => {
+  //inicialitzar mapa a figueres
+  const map = L.map('map').setView([42.2655, 2.9581], 13)
+  //afegir la capa de tiles "mapa base" amb 'OpenStreetMap
+  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',//copyright
+  }).addTo(map)
+  //Afegir marcador 
+  L.marker([42.2741, 2.9643]).addTo(map).bindPopup('Cendrassos')
+})
+</script>
