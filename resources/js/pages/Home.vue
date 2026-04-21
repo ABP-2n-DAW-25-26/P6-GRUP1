85b01f29afc25337c93c90da05b496e5d87f4308
sbaraka <sulibaraka5@gmail.com>
2026-04-21 14:35:15 +0200

    feat: add home page

---
+++ resources/js/pages/Home.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { Head } from '@inertiajs/vue3';
+import PublicLayout from '@/layouts/PublicLayout.vue';
+
+defineOptions({ layout: PublicLayout });
+</script>
+
+<template>
+    <Head title="Home" />
+    <main class="flex items-center justify-center py-24">
+        <h1 class="text-4xl font-bold text-hipatia-text">Home</h1>
+    </main>
+</template>
