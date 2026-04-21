bc459503ec98fca460f6e68fc1e385cf4321c71a
sbaraka <sulibaraka5@gmail.com>
2026-04-21 14:35:09 +0200

    feat: add public layout with navbar

---
+++ resources/js/layouts/PublicLayout.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import AppNavbar from '@/components/AppNavbar.vue';
+</script>
+
+<template>
+    <div class="min-h-screen flex flex-col bg-hipatia-bg">
+        <AppNavbar />
+        <div class="pt-24">
+            <slot />
+        </div>
+    </div>
+</template>
