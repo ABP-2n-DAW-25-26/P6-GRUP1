b30bf2d955cbb348acbd90b513d499086ce18b1e
sbaraka <sulibaraka5@gmail.com>
2026-04-22 15:20:42 +0200

    feat: exclude login page from auth layout

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -19,6 +19,8 @@ createInertiaApp({
         switch (true) {
             case name === 'Welcome':
                 return null;
+            case name === 'auth/Login':
+                return null;
             case name.startsWith('auth/'):
                 return AuthLayout;
             case name.startsWith('settings/'):
