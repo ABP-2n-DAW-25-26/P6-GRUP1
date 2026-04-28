8426f5db54015ba955b2518284e25a3f7a6e8ddb
robert-poe-gab <robert.pooenaru@gmail.com>
2026-04-28 17:05:45 +0200

    feature: db for themes

---
+++ database/migrations/2026_04_28_145158_themes.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('themes', function (Blueprint $table){
+            $table->string('name')->unique();
+            $table->string('primary', 7);
+            $table->string('primary-dark', 7);
+            $table->string('secondary', 7);
+            $table->string('text', 7);
+            $table->string('text-secondary', 7);
+            $table->string('background', 7);
+            $table->string('background-card', 7);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('themes');
+
+    }
+};
