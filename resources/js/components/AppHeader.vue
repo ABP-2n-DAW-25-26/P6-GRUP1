e2d337f9113ca17e63ec91ad9439c4455ddf1eb0
sbaraka <sulibaraka5@gmail.com>
2026-05-11 16:24:20 +0200

    Add language selector to AppHeader

--- resources/js/components/AppHeader.vue
+++ resources/js/components/AppHeader.vue
@@ -1,7 +1,8 @@
 <script setup lang="ts">
 import { Link, usePage } from '@inertiajs/vue3';
 import { BookOpen, Folder, LayoutGrid, Menu, Search } from 'lucide-vue-next';
-import { computed } from 'vue';
+import { computed, ref } from 'vue';
+import { translatePage } from '@/composables/useTranslate';
 import AppLogo from '@/components/AppLogo.vue';
 import AppLogoIcon from '@/components/AppLogoIcon.vue';
 import Breadcrumbs from '@/components/Breadcrumbs.vue';
