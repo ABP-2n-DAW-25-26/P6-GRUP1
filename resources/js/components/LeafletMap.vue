f6e97817c3183d08e0f7a6ed2afec703a5a682bb
robert-poe-gab <robert.pooenaru@gmail.com>
2026-05-18 17:35:27 +0200

    Hotfix: won't use the stadiamaps tiles

--- resources/js/components/LeafletMap.vue
+++ resources/js/components/LeafletMap.vue
@@ -70,16 +70,16 @@ onMounted(() => {
 
     map = L.map(mapElement.value).setView(center, 13);
 
-    L.tileLayer(
-        'https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.{ext}',
-        {
-            minZoom: 0,
-            maxZoom: 20,
-            attribution:
-                '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-            ext: 'png',
-        },
-    ).addTo(map);
+    // L.tileLayer(
+    //     'https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.{ext}',
+    //     {
+    //         minZoom: 0,
+    //         maxZoom: 20,
+    //         attribution:
+    //             '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    //         ext: 'png',
+    //     },
+    // ).addTo(map);
 
     createMarkerLayer();
 });
