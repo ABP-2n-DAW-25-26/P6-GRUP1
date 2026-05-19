9cc4d7d4fbd72e29cc1aa86992a4b431e752720c
robert-poe-gab <robert.pooenaru@gmail.com>
2026-05-19 15:14:20 +0200

    layers on map

--- resources/js/pages/Activities/ShowGimcana.vue
+++ resources/js/pages/Activities/ShowGimcana.vue
@@ -184,12 +184,11 @@ onMounted(async () => {
 
   L.control.zoom({ position: 'topright' }).addTo(map)
 
-  // L.tileLayer('https://tiles.stadiamaps.com/tiles/stamen_toner_lite/{z}/{x}/{y}{r}.{ext}', {
-  //   minZoom: 0,
-  //   maxZoom: 20,
-  //   attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://www.stamen.com/" target="_blank">Stamen Design</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-  //   ext: 'png',
-  // }).addTo(map)
+  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    minZoom: 0,
+    maxZoom: 20,
+    ext: 'png',
+  }).addTo(map)
 
   renderMarkers()
 })
