d7bdad8708d5701272b17dba28f20e07ee2aec82
jeffffff6 <120552004+jeffffff6@users.noreply.github.com>
2026-05-20 13:22:42 +0000

    fix pint code style

--- app/Http/Controllers/GimcanaController.php
+++ app/Http/Controllers/GimcanaController.php
@@ -53,7 +53,7 @@ class GimcanaController extends Controller
         session()->flash('message', 'Gimcana creada correctament');
 
         return to_route('exchange.show', ['exchange' => $exchange->id]);
-}
+    }
 
     public function show(Exchange $exchange, string $gimcana)
     {
