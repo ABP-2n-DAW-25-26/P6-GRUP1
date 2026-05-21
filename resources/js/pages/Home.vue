6ec03fbe067616ce67794f90b481107257118335
jeffffff6 <lolbak006@gmail.com>
2026-05-21 11:09:46 +0200

    hotfix: duplicate defineoptions()

--- resources/js/pages/Home.vue
+++ resources/js/pages/Home.vue
@@ -7,9 +7,6 @@ import { ArrowUpRight } from 'lucide-vue-next';
 import { Swiper, SwiperSlide } from 'swiper/vue';
 import { Autoplay } from 'swiper/modules';
 import 'swiper/css';
-import { ref } from 'vue';
-
-defineOptions({ layout: PublicLayout });
 
 // Regex simple para validar que el título no tenga símbolos raros
 const safeTitleRegex = /^[A-Za-z0-9\s]+$/;
