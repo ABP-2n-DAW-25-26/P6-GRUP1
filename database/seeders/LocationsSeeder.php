50b72526f445d118817832c410323bc0633a52fd
jeffffff6 <lolbak006@gmail.com>
2026-05-21 13:16:18 +0200

    feat: demo

--- database/seeders/LocationsSeeder.php
+++ database/seeders/LocationsSeeder.php
@@ -54,6 +54,7 @@ class LocationsSeeder extends Seeder
         ]);
 
         $gimcanaTitles = [
+            'Gimcana de Devs',
             'Castell Sforzesco',
             'Gimcana del centre historic de Mila',
             'Gimcana del barri de Brera',
@@ -69,31 +70,51 @@ class LocationsSeeder extends Seeder
 
         $figueresLocations = [
             [
-                'name' => 'Teatre-Museu Dali',
-                'description' => 'Museu dedicat a Salvador Dali, principal atraccio de Figueres.',
+                'name' => 'Teatre-Museu Dalí',
+                'description' => 'Museu dedicat a Salvador Dalí, principal atracció de Figueres.',
                 'latitude' => 42.2676,
                 'longitude' => 2.9606,
                 'question_type' => 'open',
-                'statement' => 'Quin artista esta vinculat a aquest museu?',
-                'correct_answer' => 'Salvador Dali',
+                'statement' => 'Quin hook de Vue s’executa just després de muntar el component?',
+                'correct_answer' => 'mounted',
             ],
             [
                 'name' => 'Castell de Sant Ferran',
-                'description' => 'Fortalesa del segle XVIII amb vistes panoramiques.',
+                'description' => 'Fortalesa del segle XVIII amb vistes panoràmiques.',
                 'latitude' => 42.2752,
                 'longitude' => 2.9553,
                 'question_type' => 'multiple_choice',
-                'statement' => 'Quin es el segle de construccio del castell?',
-                'correct_answer' => 'XVIII',
+                'statement' => 'Quin patró segueix Laravel en la seva arquitectura?',
+                'answer' => "[\"MVC\",\"MVVM\",\"MVP\",\"Flux\"]",
+                'correct_answer' => 'MVC',
             ],
             [
                 'name' => 'La Rambla de Figueres',
-                'description' => 'Zona centrica amb botigues, bars i ambient local.',
+                'description' => 'Zona cèntrica amb botigues, bars i ambient local.',
                 'latitude' => 42.2669,
                 'longitude' => 2.9633,
                 'question_type' => 'true_false',
-                'statement' => 'La Rambla es el nucli comercial de Figueres.',
+                'statement' => 'Laravel utilitza PHP com a llenguatge principal.',
                 'correct_answer' => 'true',
+            ],
+            [
+                'name' => 'Museu del Joguet de Catalunya',
+                'description' => 'Museu del joguet amb una col·lecció històrica.',
+                'latitude' => 42.2672,
+                'longitude' => 2.9629,
+                'question_type' => 'multiple_choice',
+                'statement' => 'Quina eina utilitza Laravel per gestionar dependències?',
+                'answer' => "[\"Composer\",\"NPM\",\"Yarn\",\"Vite\"]",
+                'correct_answer' => 'Composer',
+            ],
+            [
+                'name' => 'Església de Sant Pere',
+                'description' => 'Església històrica on va ser batejat Dalí.',
+                'latitude' => 42.2675,
+                'longitude' => 2.9612,
+                'question_type' => 'open',
+                'statement' => 'Quin mètode HTTP s’utilitza habitualment per actualitzar un recurs de forma parcial en una API REST?',
+                'correct_answer' => 'PATCH',
             ],
         ];
 
@@ -114,6 +135,7 @@ class LocationsSeeder extends Seeder
                     'longitude' => $location['longitude'],
                     'question_type' => $location['question_type'],
                     'statement' => $location['statement'],
+                    'answer' => $location['answer'] ?? null,
                     'correct_answer' => $location['correct_answer'],
                     'type' => 'gimcana',
                     'activity_id' => $activity->id,
